20bb1749ec7ad601a740084a5ca50fc09b911970
mazurasdf <mazurasdf@gmail.com>
2021-08-04 15:58:28 -0500

    added lecture for deck of cards

---
+++ OOP/CardDeck/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CardDeck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Deck myDeck = new Deck();
+            myDeck.Randomize();
+            myDeck.PrintDeck();
+        }
+    }
+}
