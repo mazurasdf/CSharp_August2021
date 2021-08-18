3bcce2ec05512734e1a883124127f45cfaf7e7d7
mazurasdf <mazurasdf@gmail.com>
2021-08-18 12:03:58 -0500

    first half of belt prep

---
+++ BeltPrep/Coddit/Models/LoginUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Coddit.Models
+{
+    public class LoginUser
+    {
+        [Required]
+        public string LoginUserName {get;set;}
+        [Required]
+        [DataType(DataType.Password)]
+        public string LoginPassword {get;set;}
+    }
+}
